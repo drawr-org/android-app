94514db60140d8400d868ee957687552b47812af
l.juenemann <juenemann.lukas@gmail.com>
2016-11-03 20:08:35 +0100

    created CustomEditText for login and sample preference-layout

--- app/src/main/java/de/htwb/drawr/PreferenceFragment.java
+++ app/src/main/java/de/htwb/drawr/PreferenceFragment.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Created by Lao on 03.11.2016.
@@ -13,10 +15,22 @@ import android.view.ViewGroup;
 
 public class PreferenceFragment extends Fragment {
 
+    private LinearLayout userList;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_preferences, container, false);
+        userList = (LinearLayout) view.findViewById(R.id.preference_user_list);
+
+        String[] testArray = new String[30];
+        for(int i = 0; i < testArray.length; i++) {
+            testArray[i] = "User"+(i+1);
+
+            View v = inflater.inflate(R.layout.list_item_user_preference, null, false);
+            ((TextView)v.findViewById(R.id.list_item_user_preference_name)).setText(testArray[i]);
+            userList.addView(v);
+        }
         return view;
     }
 }
